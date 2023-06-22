24f1d36faec32d3ce83c5c1d2701f8a392524b93
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-22 09:26:05 -0700

    checks if the current position is end of file

--- checker.c
+++ checker.c
@@ -9,3 +9,23 @@ void check_input(void)
 	if (isatty(STDIN_FILENO))
 		_puts(":) ");
 }
+
+/**
+ * check_EOF - checks if the current position is end of file
+ * @len: the return value of getline function
+ * @buffer: the temporal storage for holding data
+ *
+ * Return: Nothing (void)
+ */
+void check_EOF(int len, char *buffer)
+{
+	(void)buffer;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("\n");
+			free(buffer);
+		}
+	}
+}
