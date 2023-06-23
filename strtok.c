f089c848d3d9afaaf4f34087dacb1e5fe1201b6b
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-06-23 07:50:32 -0700

    Splits a string into individual token

--- strtok.c
+++ strtok.c
@@ -30,5 +30,22 @@ char **splitstr(char *str, const char *delim)
 	array = malloc((sizeof(char *) * 2));
 	array[0] = _strdup(token);
 
-
-
+	size = 3;
+	i = 1;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		array = _realloc(array, sizeof(char *) * (size - 1)),
+		      ((sizeof(char *) * size));
+		if (array == 0)
+		{
+			free(array);
+			return (NULL);
+		}
+		array[i] = _strdup(token);
+		i++;
+		size++;
+	}
+	free(input);
+	return (array);
+}
