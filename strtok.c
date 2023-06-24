373eaaaa8ceb0ca23ed28c4aa9fd8601736a3b2d
GoodnessJames <akomagoodness97@gmail.com>
2023-06-24 17:37:25 +0100

    Add strtok function

--- strtok.c
+++ strtok.c
@@ -1,27 +1,29 @@
 #include "shell.h"
 
 /**
- * splitstr - splits a string into individual tokens
- * @str: the string to be split
- * @delim: character used to indicate end of string
- * Return: on SUCCESS, returns (array) i e a pointer to an array
- * of string of token otherwise return (NULL) on failure
+ * splitStr - Splits a string into individual tokens
+ * @str: The string to be split
+ * @delim: The character used to indicate the end of a string
+ *
+ * Return: On SUCCESS, returns (array) i.e. a pointer to an array of str tokens
+ *		   On FAILURE, returns (NULL)
  */
-char **splitstr(char *str, const char *delim)
+
+char **splitStr(char *str, const char *delim)
 {
 	char **array, *token, *input;
 	int i, size;
 
-	input = malloc(strlen(str) + 1);
-	if (imput == 0)
+	input = malloc(_strlen(str) + 1);
+	if (input == 0)
 	{
 		perror(_getenv("_"));
 		return (NULL);
 	}
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		input [i] = str[i];
+		input[i] = str[i];
 		i++;
 	}
 	input[i] = '\0';
@@ -35,11 +37,10 @@ char **splitstr(char *str, const char *delim)
 	while (token)
 	{
 		token = strtok(NULL, delim);
-		array = _realloc(array, sizeof(char *) * (size - 1)),
-		      ((sizeof(char *) * size));
+		array = _realloc(array, (sizeof(char *) * (size - 1)),
+				(sizeof(char *) * size));
 		if (array == 0)
 		{
-			free(array);
 			return (NULL);
 		}
 		array[i] = _strdup(token);
